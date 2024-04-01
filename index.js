53b92389b6e281c7567cc8a38c03122da5e61718
revolyne <revolyne.mwangi@student.moringaschool.com>
2024-04-01 10:59:05 +0300

    kaka

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+        console.log('Button clicked!');
+    });
 }
+
+addingEventListener(); // Call the function to bind the event listener
